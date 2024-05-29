8d6e27ea8eebce9a19f464e4d0b96d2a941fc8f6
yuukimochizuki <y.mochizuki.92@gmail.com>
2024-05-29 21:50:51 +0900

    Todoアプリのファイル準備

---
+++ vue/src/components/TodoApp.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TodoInput from './TodoInput.vue';
+import TodoListView from './TodoListView.vue';
+</script>
+
+<template>
+  <div class="todoApp">
+    <p>Hello</p>
+    <TodoInput></TodoInput>
+    <TodoListView></TodoListView>
+  </div>
+</template>
