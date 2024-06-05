45d07c86007c2660982d01ebf5b616e337f5ea5b
yuukimochizuki <y.mochizuki.92@gmail.com>
2024-06-05 23:09:02 +0900

    フロントでタスクの一覧表示、バリデーション処理

--- vue/src/components/TodoListView.vue
+++ vue/src/components/TodoListView.vue
@@ -1,2 +1,30 @@
 <!-- Todoリスト一覧表示 -->
-<template></template>
+<script setup>
+const items = JSON.parse(localStorage.getItem("items")) || [];
+</script>
+
+<template>
+  <div>
+    <table>
+      <!-- テーブルヘッダー -->
+      <tr>
+        <th class="th-id">ID</th>
+        <th class="th-value">やること</th>
+        <th class="th-limit">期限</th>
+        <th class="th-status">状態</th>
+        <th class="th-edit">編集</th>
+        <th class="th-delete">削除</th>
+      </tr>
+
+      <!-- タスク一覧 -->
+      <tr v-for="item in items" :key="item.id">
+        <td>{{ item.id }}</td>
+        <td>{{ item.content }}</td>
+        <td>{{ item.limit }}</td>
+        <td>{{ item.state.value }}</td>
+        <td><button>編集</button></td>
+        <td><button>削除</button></td>
+      </tr>
+    </table>
+  </div>
+</template>
